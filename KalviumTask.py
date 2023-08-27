5fd9ef135fda529c44cbcbc782a282a1c0668f66
sd2153 <dsiddardhar@gmail.com>
2023-08-27 13:56:51 +0530

    Added the calculating function which does the most of the common operations

--- KalviumTask.py
+++ KalviumTask.py
@@ -1,8 +1,52 @@
-from flask import Flask
+from flask import Flask,render_template,jsonify
+import re
 
 app=Flask(__name__)
+
 @app.route("/")
-def hello():
-    return "Hello World"
+def home():
+    return "Welcome to Maths Server"
+
+@app.route("/<path:expression>", methods=['GET'])
+def calculate(expression):
+    expression = expression.replace("plus", "+").replace("minus", "-").replace("into", "*").replace("dividedby", "/").replace("power","**")
+    tokens = expression.split('/')
+
+    operands = []
+    operators = []
+
+    precedence = {"+": 1, "-": 1, "*": 2, "/": 2,"**":3}
+
+    for token in tokens:
+        if token.isdigit() or (token.startswith('-') and token[1:].isdigit()):
+            operands.append(float(token))
+        elif token in ["+", "-", "*", "/","**"]:
+            while operators and precedence.get(operators[-1], 0) >= precedence.get(token, 0):
+                operand2 = operands.pop()
+                operand1 = operands.pop()
+                operator = operators.pop()
+
+                result = eval(str(operand1) + operator + str(operand2))
+                operands.append(result)
+            operators.append(token)
+        else:
+            return {"Invalid expression"}
+
+    while operators:
+        operand2 = operands.pop()
+        operand1 = operands.pop()
+        operator = operators.pop()
+
+        result = eval(str(operand1) + operator + str(operand2))
+        operands.append(result)
+
+    question = expression
+    answer = operands[-1]
+
+    return {"question": question, "answer": answer}
+
+
+
+
 if __name__=="__main__":
     app.run(host='0.0.0.0', debug=True)
